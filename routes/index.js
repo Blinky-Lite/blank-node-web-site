981a1ad02c83d691dcf6d03c8834d7e32d26955e
David McGinnis <david.mcginnis@thequantumguys.se>
2018-08-27 08:49:05 +0200

    added google plots and colors

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Blinky-Lite', pageName: 'index'});
+  res.render('index', { title: 'Blank Web Site', pageName: 'index'});
 });
 
 module.exports = router;
